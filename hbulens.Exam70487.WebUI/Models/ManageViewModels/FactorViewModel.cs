f45c2d89b439e91969187f5db7b91d512a8e4b15
hbulens <hendrik.bulens@gmail.com>
2016-06-04 15:35:05 +0200

    Core project

---
+++ hbulens.Exam70487.WebUI/Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hbulens.Exam70487.WebUI.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
