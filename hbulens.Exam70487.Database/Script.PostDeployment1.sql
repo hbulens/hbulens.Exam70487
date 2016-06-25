fda3c55d6132fb2abcdf9566934cf3a061f5ef40
hbulens <hendrik.bulens@gmail.com>
2016-06-25 21:06:21 +0200

    Consume WCF Data Service in client application

--- hbulens.Exam70487.Database/Script.PostDeployment1.sql
+++ hbulens.Exam70487.Database/Script.PostDeployment1.sql
@@ -10,13 +10,15 @@ Post-Deployment Script Template
 --------------------------------------------------------------------------------------
 */
 
+INSERT [dbo].[Customers] ([FirstName], [LastName], [DateOfBirth]) VALUES (1, N'Hercules', N'Rockefeller', NULL)
+INSERT [dbo].[Customers] ([FirstName], [LastName], [DateOfBirth]) VALUES (2, N'Handsome B.', N'Wonderful', NULL)
+INSERT [dbo].[Customers] ([FirstName], [LastName], [DateOfBirth]) VALUES (3, N'Rembrandt Q.', N'Einstein', NULL)
+INSERT [dbo].[Customers] ([FirstName], [LastName], [DateOfBirth]) VALUES (4, N'Max', N'Power', NULL)
 
-INSERT INTO [dbo].[Customers]([FirstName],[LastName])VALUES('Hendrik','Bulens')
-INSERT INTO [dbo].[Customers]([FirstName],[LastName])VALUES('Donald','Trump')
-INSERT INTO [dbo].[Customers]([FirstName],[LastName])VALUES('James','Hetfield')
-INSERT INTO [dbo].[Customers]([FirstName],[LastName])VALUES('Kurt','Cobain')
-INSERT INTO [dbo].[Customers]([FirstName],[LastName])VALUES('Homer','Simpson')
-INSERT INTO [dbo].[Customers]([FirstName],[LastName])VALUES('Harvey','Specter')
-INSERT INTO [dbo].[Customers]([FirstName],[LastName])VALUES('Walter','White')
+INSERT [dbo].[Orders] ([CustomerId]) VALUES (1)
+INSERT [dbo].[Orders] ([CustomerId]) VALUES (2)
+INSERT [dbo].[Orders] ([CustomerId]) VALUES (3)
+INSERT [dbo].[Orders] ([CustomerId]) VALUES (4)
+
 
 GO
---
+++ hbulens.Exam70487.WcfData.Client/Program.cs
@@ -0,0 +1,33 @@
+﻿using hbulens.Exam70487.WcfData.Client.DataService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hbulens.Exam70487.WcfData.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri uri = new Uri("http://localhost:8080/ExamDataService");
+            ExamContext ctx = new ExamContext(uri);
+
+            bool tryAgain = true;
+            do
+            {
+                IEnumerable<Customer> customers = ctx.Customers.ToList();
+                Console.WriteLine(string.Format("Retrieved {0} customers from the WCF Data Service", customers.Count()));
+
+                Console.WriteLine("Re-run? (Y/N)");
+                if (Console.ReadLine() != "Y")
+                    tryAgain = false;
+            }
+            while (tryAgain);
+
+            Console.WriteLine("Press <Enter> to stop the client.");
+            Console.ReadLine();
+        }
+    }
+}
